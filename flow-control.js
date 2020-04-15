b2cfae8df26b9ab0efb8dfe339abc8a451b4c2a1
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-15 23:11:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,11 +24,10 @@ return "Conditions not met";
 }
 
 function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19) {
-  return "You are a teenager";
-  } else {return "You are not a teenager";}
+      return (age >= 13 && age <= 19) ? "You are a teenager"
+           : "You are not a teenager";
   }
-}
+
 
 function switchAge(age) {
 
